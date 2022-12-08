bcec7dae35a709e5804ef087a16c21520e80db48
TalisTalis <tsarst@gmail.com>
2022-12-08 13:27:38 +0300

    create table DB tblPages; add class for work whit DB Model-Data DB, PagesDTO and constructor PagesMV

---
+++ MVC_magic_store/Models/Data/PagesDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace MVC_magic_store.Models.Data
+{
+    // к какой таблице относится этот класс
+    [Table("tblPages")]
+    public class PagesDTO
+    {
+        // поля соответствующие столбцам таблицы. в той же последовательности
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public string Body { get; set; }
+        public int Sorting { get; set; }
+        public bool HasSideBar { get; set; }
+
+    }
+}
---
+++ MVC_magic_store/Models/ViewModels/Pages/PageMV.cs
@@ -0,0 +1,36 @@
+﻿using MVC_magic_store.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace MVC_magic_store.Models.ViewModels.Pages
+{
+    /// <summary>
+    /// Получает от DTO все данные и в конструукторе присваивает значения моделе которая пойдет на представление
+    /// </summary>
+    public class PageMV
+    {
+        public PageMV() { } // конструктор класса по умолчанию
+        public PageMV(PagesDTO row) { // конструктор класса который будет присваивать моделе значения из pagesDTO 
+            Id = row.Id;
+            Title= row.Title;
+            Slug= row.Slug;
+            Body= row.Body;
+            Sorting= row.Sorting;
+            HasSideBar= row.HasSideBar;
+        } 
+        public int Id { get; set; }
+        [Required] // обязательное для заполнения поле
+        [StringLength(50,MinimumLength = 3)] // максимальное и минимальное количество символов
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        [Required]
+        [StringLength(int.MaxValue,MinimumLength = 3)]
+        public string Body { get; set; }
+        public int Sorting { get; set; }
+        public bool HasSideBar { get; set; }
+    }
+}
