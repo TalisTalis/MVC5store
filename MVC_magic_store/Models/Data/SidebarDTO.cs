c93a413d2ca1622d9057e2339fa0d82f483b9d40
TalisTalis <tsarst@gmail.com>
2022-12-11 19:14:46 +0300

    add table sidbar and sidebarDTO

---
+++ MVC_magic_store/Models/Data/SidebarDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVC_magic_store.Models.Data
+{
+    [Table("tblSidebar")]
+    public class SidebarDTO
+    {
+        // реализовать автоматические свойства с гетером и сетером по типу полей из БД
+        [Key]
+        public int Id { get; set; }
+
+        public string Body { get; set; }
+    }
+}
