2bfc8c37257144fc78fb5fb1de7950130d424508
TalisTalis <tsarst@gmail.com>
2022-12-15 23:26:46 +0300

    add metod add new category name and delete gategory ajax

--- MVC_magic_store/Areas/Admin/Controllers/ShopController.cs
+++ MVC_magic_store/Areas/Admin/Controllers/ShopController.cs
@@ -33,5 +33,107 @@ namespace MVC_magic_store.Areas.Admin.Controllers
             // Возвращаем в представление
             return View(categoryVMList);
         }
+
+        // POST: Admin/Shop/AddNewCategory
+        [HttpPost]
+        public string AddNewCategory(string catName)
+        {
+            // план:
+            // объявить строковую переменную id
+            // проверить имя категории на уникальность
+            // инициализировать модель dto
+            // добавить данные в модель
+            // сохранить в БД
+            // получить id чтобы вернуть в представление
+            // возвращение id в представление
+
+            string id;
+
+            // подключение к БД
+            using(DB db = new DB())
+            {
+                // проверка на уникаальность названия категории
+                if (db.Categories.Any(x => x.Name == catName))
+                {
+                    return "titletaken"; // если название такое уже есть то возвращает кодовое слово
+                }
+
+                // инициализация модели DTO
+                CategoryDTO dto = new CategoryDTO();
+
+                // Добавление данные в модель
+                dto.Name = catName;
+                dto.Slug = catName.Replace(" ", "-").ToLower();
+                dto.Sorting = 100;
+
+                // Сохранение
+                db.Categories.Add(dto);
+                db.SaveChanges();
+
+                // Получение id того что сохрании
+                id = dto.Id.ToString();
+            }
+
+            // возвращение в представление
+            return id;
+        }
+        
+        // метод сортировки страниц
+        // POST: Admin/Shop/ReorderCategories
+        [HttpPost]
+        public void ReorderCategories(int[] id)
+        {
+            // план:
+            // создаем счетчик count 
+            // инициализируем модель данных
+            // устанавливаем сортировку ддля каждой страницы (в БД есть метод Sorting)
+
+            // Открываем подключение к БД
+            using (DB db = new DB())
+            {
+                int count = 1; // home по умолчанию равен 0, поэтому отсчёт начинается с 1
+
+                // инициализируем модель данных
+                CategoryDTO dto;
+
+                // Сортировка страниц
+                foreach (var categoryId in id)
+                {
+                    dto = db.Categories.Find(categoryId); // ищем элемент по id
+                    dto.Sorting = count; // присваиваем текущему элементу Sorting 1
+
+                    db.SaveChanges(); // сохраняем изменения
+
+                    count++;
+                }
+            }
+        }
+
+        // метод удаления категории
+        // POST: Admin/Shop/DeleteCategory
+        public string DeleteCategory(int id)
+        {
+            // план:
+            // получаем страницу
+            // удаем страницу
+            // сохранение изменений в БД
+            // переадресация на страницу index
+
+            // Поддключение к БД
+            using (DB db = new DB())
+            {
+                // Получение страницы
+                CategoryDTO dto = db.Categories.Find(id);
+
+                // Удаление страницы
+                db.Categories.Remove(dto);
+
+                // Сохранение изменений
+                db.SaveChanges();
+            }
+
+            // Переадресация на страницу Categories
+            return id.ToString();
+        }
     }
 }
