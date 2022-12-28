047aafb00902fb232e449a24cfbeb166b20ec858
TalisTalis <tsarst@gmail.com>
2022-12-28 13:16:53 +0300

    add metod category outpu products by category

--- MVC_magic_store/Controllers/ShopController.cs
+++ MVC_magic_store/Controllers/ShopController.cs
@@ -39,5 +39,65 @@ namespace MVC_magic_store.Controllers
 
             return PartialView("_CategoryMenuPartial", categoryVMList);
         }
+
+        // метод вывода товаров по категориям
+        // GET: Shop/Category/name
+        public ActionResult Category(string name)
+        {
+            // план:
+            // объявить список типа лист
+            // получение id категории
+            // инициализация списка данными
+            // получение имени категории
+            // проверка на null
+            // возврещение представления с моделью
+
+            // Объявление списка типа List
+            List<ProductVM> productVMList;
+
+            // Получение id категории
+            // Подключение к БД
+            using (DB db = new DB())
+            {
+                // объявление и инициализация найденой в БД по имени категории
+                CategoryDTO categoryDTO = db.Categories.Where(x => x.Slug == name).FirstOrDefault();
+
+                // получение id
+                int catId = categoryDTO.Id;
+
+                // инициализация списка данными
+                productVMList = db.Products.ToArray().Where(x => x.CategoryId == catId).Select(x => new ProductVM(x)).ToList();
+
+                // получение имени категории
+                var productCat = db.Products.Where(x => x.CategoryId == catId).FirstOrDefault();
+
+                // проверка на null
+                if (productCat == null)
+                {
+                    // если нет еще в категории ни одного товара
+                    // поиск имени категории не в товарах, а в категориях
+                    var catName = db.Categories.Where(x => x.Slug == name).Select(x => x.Name).FirstOrDefault();
+
+                    ViewBag.CategoryName = catName;
+                }
+                else
+                {
+                    ViewBag.CategoryName = productCat.CategoryName;
+                }
+            }
+
+            // возвращение модели в представление
+            return View(productVMList);
+        }
+
+        // метод вывода товаров по категориям
+        // GET: Shop/product-details/name
+        // ссылка на метод отличается от названия метода
+        // поэтому указывается в аннотации по какому названию можно найти этот метод
+        [ActionName("product-details")]
+        public ActionResult ProductDetails(string name)
+        {
+
+        }
     }
 }
